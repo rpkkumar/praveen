75be6af28a755a35063c7dcdc905093674607740
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-01-21 15:14:07 +0530

    Create praveen1.c

---
+++ praveen1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("A");
+scanf("/t%d",&a);
+if(a<0)
+{
+printf("/n negative");
+}
+else if(a=0)
+{
+printf("/n zero");
+}
+else
+{
+printf("/n negative");
+}
+}
